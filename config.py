27020bcb7325f058534b5dd9ba24aa0f979f9d21
abishekasandaru <84671985+abishekasandaru@users.noreply.github.com>
2021-12-16 06:48:06 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    BOT_TOKEN = "2121319469:AAGuY7rxo9o6c8UF-KoR1h5k1oXw3CC1tKE" # 🌚 get it from @botfather (telegram) by sending /newbot command.
+    API_ID = 6065291  # 🌚 Same as APP_ID. Get it from my.telegram.org.
+    API_HASH = "dc7873c0a5c737af4356d4f245fe696d" # 🌚 Get it from my.telegram.org.
+    
